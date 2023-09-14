79cfacbbb175f5fb602bf40507b3e366e8e129cb
SkywalkerDarth <danteuche@gmail.com>
2023-09-14 14:34:53 +0100

    app.py

--- server/app.py
+++ server/app.py
@@ -1,34 +1,33 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
-from flask_login import LoginManager, login_user, login_required, UserMixin
+from flask_login import LoginManager, login_user, login_required, UserMixin, logout_user, current_user
 from user_manager import UserManager
 
 app = Flask(__name__)
-app.secret_key = '111'
+app.secret_key = 'your_secret_key_here'  # Replace with a secure secret key
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 user_manager = UserManager('users.json')
 
 class User(UserMixin):
-    def __init__(self, user_id, username):
+    def __init__(self, user_id, username, password):
         self.id = user_id
         self.username = username
+        self.password = password
+
 
 @login_manager.user_loader
 def load_user(user_id):
     user_data = user_manager.get_user_by_id(user_id)
     if user_data:
-        return User(user_data['id'], user_data['username'])
+        return User(user_data['id'], user_data['username'], user_data['password'])
     return None
-
 
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -39,16 +38,13 @@ def login():
         user = user_manager.get_user_by_username(username)
 
         if user and user_manager.check_password(user['password'], password):
-            login_user(User(user['id'], user['username']))
+            login_user(User(user['id'], user['username'], user['password']))
             flash('Sign in successful!', 'success')
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid username or password. Please try again.', 'error')
-            return render_template('login.html')
 
     return render_template('login.html')
-
-
 
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -56,51 +52,78 @@ def signup():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        users = user_manager.read_users()
+        if user_manager.get_user_by_username(username):
+            flash('Username already taken, please try another username', 'error')
+        else:
+            user_id = str(user_manager.generate_user_id())
+            hashed_password = user_manager.hash_password(password)
+            user = {'id': user_id, 'username': username, 'password': hashed_password}
 
-        if any(user['username'] == username for user in users):
-            return render_template('signup.html', error='Username already taken, please try another username')
-
-        user_id = str(len(users) + 1)
-        hashed_password = user_manager.hash_password(password)
-        user = {'id': user_id, 'username': username, 'password': hashed_password}
-
-        user_manager.write_user(user)
-        flash('Signup successful! Please log in.', 'success')
-
-        return redirect(url_for('login'))
+            user_manager.write_user(user)
+            flash('Signup successful! Please log in.', 'success')
+            return redirect(url_for('login'))
 
     return render_template('signup.html')
-
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', username=current_user.username)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'success')
+    return redirect(url_for('index'))
+
+@app.route('/change_password', methods=['POST', 'GET'])
+@login_required
+def change_password():
+    if request.method == 'POST':
+        old_password = request.form.get('old_password')
+        new_password = request.form.get('new_password')
+
+        if user_manager.check_password(current_user.password, old_password):
+            new_hashed_password = user_manager.hash_password(new_password)
+            user_manager.update_password(current_user.username, new_hashed_password)
+            flash('Password changed successfully!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid old password. Please try again.', 'error')
+
+    return render_template('change_password.html')
 
 
+@login_required
 @app.route('/engineering')
 def engineering():
     return render_template('engineering.html')
 
 
+@login_required
 @app.route('/history')
 def history():
     return render_template('history.html')
 
 
+@login_required
 @app.route('/stories')
 def stories():
     return render_template('stories.html')
 
 
+@login_required
 @app.route('/sciences')
 def sciences():
     return render_template('sciences.html')
 
+
+@login_required
 @app.route('/sports')
 def sports():
     return render_template('sports.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/user_manager.py
+++ server/user_manager.py
@@ -1,7 +1,6 @@
 import json
 from flask_bcrypt import Bcrypt
 
-
 class UserManager:
     def __init__(self, data_file):
         self.DATA_FILE = data_file
@@ -15,37 +14,31 @@ class UserManager:
         except (FileNotFoundError, json.JSONDecodeError):
             return []
 
-    def write_user(self, user_data):
-        users = self.read_users()
-        users.append(user_data)
+    def write_users(self, users):
         with open(self.DATA_FILE, 'w') as file:
             json.dump({'users': users}, file)
 
     def get_user_by_id(self, user_id):
-        try:
-            with open(self.DATA_FILE, 'r') as file:
-                users_data = json.load(file)
-                return users_data.get(str(user_id))
-        except (FileNotFoundError, json.JSONDecodeError):
-            print("Error: Unable to decode JSON data")
-        return None
+        users = self.read_users()
+        return next((user for user in users if user['id'] == user_id), None)
 
     def get_user_by_username(self, username):
-        try:
-            with open(self.DATA_FILE, 'r') as file:
-                users_data = json.load(file)
-                for user in users_data.get('users', []):
-                    if user['username'] == username:
-                        return user
-        except (FileNotFoundError, json.JSONDecodeError):
-            print("Error: Unable to decode JSON data")
-        return None
+        users = self.read_users()
+        return next((user for user in users if user['username'] == username), None)
+
+    def update_password(self, username, new_hashed_password):
+        users = self.read_users()
+        updated_users = [{'id': user['id'], 'username': user['username'], 'password': new_hashed_password} if user['username'] == username else user for user in users]
+        self.write_users(updated_users)
 
     def delete_user(self, username):
         users = self.read_users()
         updated_users = [user for user in users if user['username'] != username]
-        with open(self.DATA_FILE, 'w') as file:
-            json.dump({'users': updated_users}, file)
+        self.write_users(updated_users)
+
+    def generate_user_id(self):
+        users = self.read_users()
+        return str(len(users) + 1)
 
     def hash_password(self, password):
         return self.bcrypt.generate_password_hash(password).decode('utf-8')
