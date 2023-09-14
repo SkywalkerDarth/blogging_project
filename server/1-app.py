3ab40f0f0977830fd63df7953548f9ce0aa1f2af
SkywalkerDarth <danteuche@gmail.com>
2023-09-14 08:40:00 +0100

    all

---
+++ server/1-app.py
@@ -0,0 +1,161 @@
+from flask import Flask, render_template, request, flash, redirect, session, url_for
+import json
+import os
+from flask_login import LoginManager, login_user, login_required
+from flask_bcrypt import Bcrypt
+
+
+app = Flask(__name__)
+app.secret_key = ''
+bcrypt = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+DATA_FILE = 'users.json'
+
+
+class User:
+    def __init__(self, username):
+        self.username = username
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return self.username
+
+
+def get_user_data_by_username(username):
+    try:
+        with open('DATA_FILE', 'r') as file:
+            users_data = json.load(file)['users']
+            for user_data in users_data:
+                if user_data['username'] == username:
+                    return user_data
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("Error: Unable to decode JSON data")
+
+    return None
+
+
+
+def load_user(username):
+    # Load the user based on the username
+    user_data = get_user_data_by_username(username)
+    if user_data:
+        return User(user_data['username'])
+    return None
+
+
+
+def read_users():
+    try:
+        with open(DATA_FILE, 'r') as file:
+            users = json.load(file)
+            return users.get('users', [])
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+
+def write_users(users):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(users, file)
+
+
+
+def delete_users(username):
+    users = read_users()
+    updated_users = [user for user in users if user['username'] != username]
+    write_users(updated_users)
+
+
+#Routes#
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Retrieve the users from the data file
+        users = read_users()
+
+        # Check if the username exists
+        user = next((user for user in users if user['username'] == username), None)
+
+        if user and check_password_hash(user['password'], password):
+            # If the username and password are correct, log the user in
+            login_user(User(user['username']))
+
+            return redirect(url_for('dashboard'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    # If GET request, render the login page
+    return render_template('login.html')
+
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')     
+        users = read_users()
+
+        if any(user['username'] == username for user in users):
+            return render_template('signup.html', error='Username already taken, please try another username')
+
+        # Hash the password
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        # Create a new user object
+        user = {'username': username, 'password': hashed_password}
+
+        # Add the user to the list of users
+        users.append(user)
+
+        # Write the updated list of users to the file
+        write_users(users)
+
+        return render_template('login.html')
+
+    return render_template('signup.html')
+
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
+
+
+
+@app.route('/recovery')
+def recovery():
+    return render_template('recovery.html')
+
+
+
+@app.route('/login/first_page')
+def first_page():
+    return render_template('first_page.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- server/app.py
+++ server/app.py
@@ -1,57 +1,41 @@
-from flask import Flask, render_template, request, flash, redirect, session, url_for
-import json
-import os
-from flask_login import LoginManager, login_user, login_required
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_login import LoginManager, login_user, login_required, UserMixin
 from flask_bcrypt import Bcrypt
+import json
 
 
 app = Flask(__name__)
-app.secret_key = ''
+app.secret_key = '111'
 bcrypt = Bcrypt(app)
-DATA_FILE = 'users.json'
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 
-class User:
-    def __init__(self, username):
+DATA_FILE = 'users.json'
+
+
+class User(UserMixin):
+    def __init__(self, user_id, username):
+        self.id = user_id
         self.username = username
 
-    def is_authenticated(self):
-        return True
 
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.username
+@login_manager.user_loader
+def load_user(user_id):
+    user_data = get_user_by_id(user_id)
+    if user_data:
+        return User(user_data['id'], user_data['username'])
+    return None
 
 
-def get_user_data_by_username(username):
+def get_user_by_id(user_id):
     try:
-        with open('DATA_FILE', 'r') as file:
-            users_data = json.load(file)['users']
-            for user_data in users_data:
-                if user_data['username'] == username:
-                    return user_data
+        with open(DATA_FILE, 'r') as file:
+            users_data = json.load(file)
+            return users_data.get(str(user_id))
     except (FileNotFoundError, json.JSONDecodeError):
         print("Error: Unable to decode JSON data")
-
     return None
-
-
-
-def load_user(username):
-    # Load the user based on the username
-    user_data = get_user_data_by_username(username)
-    if user_data:
-        return User(user_data['username'])
-    return None
-
 
 
 def read_users():
@@ -63,26 +47,27 @@ def read_users():
         return []
 
 
-
-def write_users(users):
-    with open(DATA_FILE, 'w') as file:
-        json.dump(users, file)
-
-
-
-def delete_users(username):
+def write_user(user_id, user_data):
     users = read_users()
-    updated_users = [user for user in users if user['username'] != username]
-    write_users(updated_users)
+    users.append(user_data)
+    with open(DATA_FILE, 'w') as file:
+        json.dump({'users': users}, file)
 
 
-#Routes#
+def delete_user(user_id):
+    users = read_users()
+    if user_id in users:
+        del users[user_id]
+        with open(DATA_FILE, 'w') as file:
+            json.dump(users, file)
+        return True
+    else:
+        return False
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -91,48 +76,39 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        # Retrieve the users from the data file
-        users = read_users()
+        user = get_user_by_id(user_id)
 
-        # Check if the username exists
-        user = next((user for user in users if user['username'] == username), None)
-
-        if user and check_password_hash(user['password'], password):
-            # If the username and password are correct, log the user in
-            login_user(User(user['username']))
-
+        if user and bcrypt.check_password_hash(user['password'], password):
+            login_user(User(user['id'], user['username']))
+            flash('Sign in successful!', 'success')
             return redirect(url_for('dashboard'))
         else:
-            return render_template('login.html', error='Invalid username or password')
+            flash('Invalid username or password. Please try again.', 'error')
+            return render_template('login.html')
 
-    # If GET request, render the login page
     return render_template('login.html')
 
 
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
         username = request.form.get('username')
-        password = request.form.get('password')     
+        password = request.form.get('password')
+
         users = read_users()
 
         if any(user['username'] == username for user in users):
             return render_template('signup.html', error='Username already taken, please try another username')
 
-        # Hash the password
+        user_id = str(len(users) + 1)
         hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        user = {'id': user_id, 'username': username, 'password': hashed_password}
 
-        # Create a new user object
-        user = {'username': username, 'password': hashed_password}
+        write_user(user_id, user)
+        flash('Signup successful! Please log in.', 'success')
 
-        # Add the user to the list of users
-        users.append(user)
-
-        # Write the updated list of users to the file
-        write_users(users)
-
-        return render_template('login.html')
+        return redirect(url_for('login'))
 
     return render_template('signup.html')
 
@@ -143,18 +119,14 @@ def signup():
 def dashboard():
     return render_template('dashboard.html')
 
-
-
 @app.route('/recovery')
 def recovery():
     return render_template('recovery.html')
-
-
 
 @app.route('/login/first_page')
 def first_page():
     return render_template('first_page.html')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
+
